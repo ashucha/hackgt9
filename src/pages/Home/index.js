f49486b11f6983da142d2a266cc8d5e8de8ae267
Ryan Patrick O'meara <ryanomeara12@gmail.com>
2022-10-23 05:17:59 -0400

    Made navbar and intro modal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import NavBar from "../../components/NavBar";
+import IntroModal from "../../components/IntroModal";
 
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center">Home</h1>
+			<div style={{borderBottom: '1px solid white'}}><NavBar></NavBar></div>
+			<h1 style={{textAlign: 'center'}}>Home</h1>
 		</div>
 	);
 };
